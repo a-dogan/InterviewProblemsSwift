2d8ca6cee3b2e75807851f9c83e84305bbd2006b
Art Dogan <artdog@amazon.com>
2015-11-01 16:15:47 -0800

    Rotating a Character array
    Change-Id: I70689a63d68bd096fa347de1b5158922559463de

--- Cracking the Code.playground/Pages/scratch.xcplaygroundpage/Contents.swift
+++ Cracking the Code.playground/Pages/scratch.xcplaygroundpage/Contents.swift
@@ -18,3 +18,25 @@ if (d["hello"] != nil) {
 }
 
 
+
+func rotateArray(input:[[Character]]) {
+    var transposed = [[Character]]()
+    let rowLength = input[0].count
+    
+    transposed.append(input[0])
+    transposed.append(input[1])
+    
+    for var i=0; i < input.count; i++ {
+        for var j=0; j < input[i].count; j++ {
+            transposed[rowLength - i - 1][j] = input[j][i]
+        }
+        
+    }
+    
+    transposed
+}
+
+
+var inputArray:[[Character]] = [["a","b"],["c","d"]]
+
+rotateArray(inputArray)
